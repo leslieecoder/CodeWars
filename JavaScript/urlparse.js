2914e290cd5bbd57d5f06b199a2377e80c5e1292
Lessleya <cru19010@byui.edu>
2024-12-13 19:15:03 -0700

    Kata of the day. You should hire me :)

---
+++ JavaScript/urlparse.js
@@ -0,0 +1,25 @@
+// Write a function that when given a URL as a string, parses out just the domain name and returns it as a string. For example:
+
+//* url = "http://github.com/carbonfive/raygun" -> domain name = "github"
+
+// url => string
+// return => string
+// "http://github.com/carbonfive/raygun" -> domain name = "github"
+
+
+function domainName(url){
+    
+
+    // Remove protocol (http://, https://, etc.) and "www."
+    url = url.replace(/(https?:\/\/)?(www\.)?/, "");
+
+    // Split by "/" to ignore paths or queries
+    url = url.split("/")[0];
+
+    // Split by "." and return the first part
+    const parts = url.split(".");
+    return parts[0]; // This is the domain name
+}
+  
+
+  
